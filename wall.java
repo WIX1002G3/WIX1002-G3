46705b664ea253830fb4f45b602bddc740cd8610
deathxexm <51823045+deathxexm@users.noreply.github.com>
2020-12-27 21:26:11 +0800

    Update wall.java

--- wall.java
+++ wall.java
@@ -2,4 +2,20 @@ import java.util.Scanner;
 
 public class wall {
     private int unitWall,hp;
+    
+    public void setUnitWall {
+        this.unitWall = unitWall;
+    }
+    
+    public void setHp {
+        this.hp = hp;
+    }
+    
+    public void getUnitWall {
+        return unitWall;
+    }
+    
+    public void getHp {
+        return hp;
+    }
 }
