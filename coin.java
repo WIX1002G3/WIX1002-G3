85a354d06234b4f8618db17865c0862bc4d36e75
deathxexm <51823045+deathxexm@users.noreply.github.com>
2020-12-29 21:11:52 +0800

    Update coin.java

--- coin.java
+++ coin.java
@@ -1,1 +1,11 @@
-
+public class coin {
+    private int totalCoin = 50; //initial coin
+    //Get current value of coin
+    public int getTotalCoin() {
+        return totalCoin;
+    }
+    //Updates value of coin
+    public void setTotalCoin(int coinChg) {
+        totalCoin = totalCoin + coinChg;
+    }
+}
