fea1b0aefdfc12b2ce7e4722003e8fcebea49efb
deathxexm <51823045+deathxexm@users.noreply.github.com>
2021-01-04 20:33:21 +0800

    Update cannon.java

--- cannon.java
+++ cannon.java
@@ -5,16 +5,16 @@ public class cannon extends weapon {
     public int getAtkDmg(int row) {
         lvl = super.getLevel(row);
         switch (lvl) {
-            case 1 ->                 { //Level 1 damage 1
+            case 1 ->                 { //Level 1 damage 15
                     int atkdmg = 15;
                 }
-            case 2 ->                 { //Level 2 damage 2
+            case 2 ->                 { //Level 2 damage 20
                     int atkdmg = 20;
                 }
-            case 3 ->                 { //Level 3 damage 3
+            case 3 ->                 { //Level 3 damage 25
                     int atkdmg = 25;
                 }
-            default ->                 {    //Level 4 damage 4
+            default ->                 {    //Level null damage 0
                     int atkdmg = 0;
                 }
         }
