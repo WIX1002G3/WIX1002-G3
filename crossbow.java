f8110051e6a27f333d9dcb3be9fb99a638185bc2
deathxexm <51823045+deathxexm@users.noreply.github.com>
2021-01-04 20:20:53 +0800

    Create crossbow.java

---
+++ crossbow.java
@@ -0,0 +1,26 @@
+//Level 1, 2 coins    Level 2, 5 coins    Level 3, 10 coins
+package wix1002assignment;
+
+public class crossbow extends weapon {
+    private int atkdmg,lvl;     //atkdmg for this crossbow, lvl for specified row weapon level
+    
+    public int getAtkDmg(int row) {
+        lvl = super.getLevel(row);
+        switch (lvl) {
+            case 1 ->                 { //Level 1 damage 1
+                    int atkdmg = 2;
+                }
+            case 2 ->                 { //Level 2 damage 2
+                    int atkdmg = 5;
+                }
+            case 3 ->                 { //Level 3 damage 3
+                    int atkdmg = 10;
+                }
+            default ->                 {    //Level 4 damage 4
+                    int atkdmg = 0;
+                }
+        }
+        
+        return atkdmg; 
+    }
+}
